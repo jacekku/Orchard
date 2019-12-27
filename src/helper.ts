6e74745c7a78e85e9446dd5af3322782bea40b6a
jacekku <kecajwy@gmail.com>
2019-12-27 23:09:55 +0100

    json image config handling

--- src/helper.ts
+++
@@ -1,8 +0,0 @@
-function importFromJson(file: string, callback: Function) {
-	fetch(file)
-		.then(e => e.json()
-			.then(json => {
-				callback(json)
-			})
-		)
-}
--- src/tree.ts
+++ src/tree.ts
@@ -3,13 +3,22 @@ import * as p5 from 'p5'
 import { GraphicsHandler } from './graphicsHandler'
 
 export class Tree extends GameObject {
+    growthStage: number
+
     constructor(x: number, y: number) {
         super(x, y)
+        this.growthStage = 0
     }
 
 
     show(p: p5) {
-        const img: p5.Image = GraphicsHandler.instance.getImage('asdf')
+        const img: p5.Image = GraphicsHandler.instance.getImage('oak_tree', this.growthStage)
         p.image(img, this.x, this.y)
+    }
+    tick(){
+        super.tick()
+        if (this.age % 100 === 0 && this.growthStage < 9) {
+            this.growthStage += 1 
+        }
     }
 }
